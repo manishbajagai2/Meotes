17b30e6611657de843a5f6e14989a47e9e87a9e9
Manish Bajagai <manishbajagi2@gmail.com>
2023-05-08 03:32:05 +0530

    Routing set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Navigate, Route, Routes } from "react-router-dom"
 
 function App() {
-
-  return (
-      <>
-        <div>
-          <h1>Hello World</h1>
-        </div>
-      </>
-  )
+    return (
+        <Container className="my-4">
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/new" element={<h1>New</h1>} />
+                <Route path="/:id">
+                    <Route index element={<h1>Show</h1>} />
+                    <Route path="edit" element={<h1>Edit</h1>} />
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Container>
+    )
 }
 
 export default App
